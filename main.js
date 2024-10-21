53542398e515d349d448fd12aa01eedb3caf5661
andre8624 <161709690+andre8624@users.noreply.github.com>
2024-10-21 08:14:25 -0300

    .

--- main.js
+++ main.js
@@ -15,5 +15,12 @@ function criaCartao (categoria, pergunta, resposta){
     `
 
  container.appendChild(cartao);
+ let respostaEstaVisivel
+    function viraCartao(){
+        respostaEstaVisivel = !respostaEstaVisivel;
+        cartao.classList.toggle('active',respostaEstaVisivel);
+    }
+cartao.addEventListener('click',viraCartao)
+    container.appendChild(cartao);
   
 }
