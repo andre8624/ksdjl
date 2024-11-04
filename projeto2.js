4ecccde62dd3283e36ad8625d1bbec8f1c996a59
andre8624 <161709690+andre8624@users.noreply.github.com>
2024-11-04 07:50:55 -0300

    .

--- projeto2.js
+++ projeto2.js
@@ -21,13 +21,13 @@ function criaCartao (categoria, pergunta, resposta){
 function mostraReposta (card)   {
     const resposta=document.querySelector ('.resposta-cartao')
 if(resposta.Style.display === 'none'){
-resposta.style.display = 'black';
+resposta.style.display = 'block';
 
 
 
 }
     else(resposta.Style.display === 'none'){
-        resposta.style.display = 'black';
+        resposta.style.display = 'block';
 
 
     }
