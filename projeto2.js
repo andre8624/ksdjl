3e4234794de555a4fe51bab9526a2ad67d952333
andre8624 <161709690+andre8624@users.noreply.github.com>
2024-11-04 07:49:14 -0300

    .

--- projeto2.js
+++ projeto2.js
@@ -3,7 +3,7 @@ function criaCartao (categoria, pergunta, resposta){
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
     cartao.innerHTML = `
-    <div class="conteudo-cartao">
+    <div class="conteudo-cartao" onclick = "mostraReposta (this)">
     <h3>${categoria}</h3>
     <div class="pergunta-cartao">
         <p>${pergunta}</p>
@@ -17,4 +17,17 @@ function criaCartao (categoria, pergunta, resposta){
  container.appendChild(cartao);
 
   
+}
+function mostraReposta (card)   {
+    const resposta=document.querySelector ('.resposta-cartao')
+if(resposta.Style.display === 'none'){
+resposta.style.display = 'black';
+
+
+
+}
+    else(resposta.Style.display === 'none'){
+
+
+    }
 }
